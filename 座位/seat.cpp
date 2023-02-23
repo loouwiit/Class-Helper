fd8b6786eb2dd1b8199d17a62ccee8d973068c2a
lo <loouwiit@outlook.com>
2023-02-23 13:54:06 +0800

    null seat.dll加载文件时的错误显示 正常显示还没有写

--- 座位/seat.cpp
+++ 座位/seat.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "Interface.h"
 #include "Button.h"
 
@@ -14,14 +15,17 @@ void event_Key(sf::Event::KeyEvent key);
 void event_Mouse(sf::Event::MouseMoveEvent mouse);
 void event_Mouse(sf::Event::MouseButtonEvent mouse);
 
-using Function = Interface::Function;
+void seats_Load(const char Path[]);
+void seats_Clicked(unsigned index);
 
-constexpr int text_Nunber = 2;
+using Function = Interface::Function;
 
 sf::Font font;
 Interface_Dll* self = nullptr;
 sf::RenderTexture* texture;
-Button_Text text[text_Nunber];
+Button_Text text_Exit;
+Button_Text* seats;
+unsigned seats_Number = 0;
 sf::Color background_Color = sf::Color(0x99FFFFFF);
 
 DLL void* init(void* self)
@@ -33,23 +37,17 @@ DLL void* init(void* self)
 
 	if (!font.loadFromFile("C:\\Windows\\Fonts\\msyh.ttc")) font.loadFromFile("C:\\Windows\\Fonts\\msyh.ttf");
 
-	text[1].get_Text().setFont(font);
-	text[1].get_Text().setString(L"退出seat.dll");
-	text[1].get_Text().setFillColor(sf::Color(0xFFFFFFFF));
-	text[1].get_Text().setPosition((float)(1920 - 50), (float)50);
-	text[1].get_Text().setOrigin(text[1].get_Text().getGlobalBounds().width, 0.0f); //右上对齐
-	text[1].init();
-	text[1].set_Default_Color(sf::Color(0x0));
-	text[1].set_High_Light_Color(sf::Color(0x33333366));
+	text_Exit.get_Text().setFont(font);
+	text_Exit.get_Text().setString(L"退出seat.dll");
+	text_Exit.get_Text().setFillColor(sf::Color(0xFFFFFFFF));
+	text_Exit.get_Text().setPosition((float)(1920 - 50), (float)50);
+	text_Exit.get_Text().setOrigin(text_Exit.get_Text().getGlobalBounds().width, 0.0f); //右上对齐
+	text_Exit.init();
+	text_Exit.set_Default_Color(sf::Color(0x0));
+	text_Exit.set_High_Light_Color(sf::Color(0x33333366));
 
-	text[0].get_Text().setFont(font);
-	text[0].get_Text().setString(L"这里是seat");
-	text[0].get_Text().setFillColor(sf::Color(0xFFFFFFFF));
-	text[0].get_Text().setPosition((float)(1920 / 2), (float)(1080 / 2));
-	text[0].get_Text().setOrigin(text[0].get_Text().getGlobalBounds().width / 2, text[0].get_Text().getGlobalBounds().height / 2);
-	text[0].init();
-	text[0].set_Default_Color(sf::Color(0x0));
-	text[0].set_High_Light_Color(sf::Color(0x33333366));
+	setlocale(LC_ALL, "chs");
+	seats_Load(".\\test.test哈哈哈刺激");
 
 	return nullptr;
 }
@@ -96,8 +94,11 @@ DLL void* compute(void* null)
 DLL void* draw(void* null)
 {
 	texture->clear(background_Color);
-	for (int i = 0; i < text_Nunber; i++)
-		texture->draw(text[i]);
+
+	for (unsigned i = 0; i < seats_Number; i++)
+		texture->draw(seats[i]);
+
+	texture->draw(text_Exit);
 	return nullptr;
 }
 
@@ -109,6 +110,12 @@ DLL void* draw(void* null)
 
 void ened()
 {
+	if (seats != nullptr)
+	{
+		seats_Number = 0;
+		delete[] seats;
+	}
+
 	self->set_Running(false);
 
 	printf("seat::ened\n");
@@ -135,16 +142,72 @@ void event_Key(sf::Event::KeyEvent key)
 
 void event_Mouse(sf::Event::MouseMoveEvent mouse)
 {
-	for (int i = 0; i < text_Nunber; i++)
-		text[i].set_High_Light(text[i].is_Clicked({ (float)mouse.x,(float)mouse.y }));
+	sf::Vector2f mouse_f{ (float)mouse.x,(float)mouse.y };
+
+	text_Exit.set_High_Light(text_Exit.is_Clicked(mouse_f));
+
+	for (unsigned i = 0; i < seats_Number; i++)
+		seats[i].set_High_Light(seats[i].is_Clicked(mouse_f));
 }
 
 void event_Mouse(sf::Event::MouseButtonEvent mouse)
 {
+	sf::Vector2f mouse_f{ (float)mouse.x,(float)mouse.y };
 
-	if (text[1].is_Clicked({ (float)mouse.x,(float)mouse.y }))
+	if (text_Exit.is_Clicked(mouse_f))
 	{
 		self->set_Next_Dll(".\\dll\\loader.dll");
 		ened();
 	}
+
+	for (unsigned i = 0; i < seats_Number; i++)
+		if (seats[i].is_Clicked(mouse_f)) seats_Clicked(i);
+}
+
+void seats_Load(const char Path[])
+{
+	std::ifstream file;
+	file.open(Path);
+	if (!file.is_open())
+	{
+		printf("seat::seats_Load:加载%s失败\n", Path);
+
+		if (seats != nullptr)
+		{
+			seats_Number = 0;
+			delete[] seats;
+		}
+
+		wchar_t buffer[200] = L"";
+		mbstowcs_s(NULL, buffer, sizeof(buffer) / sizeof(wchar_t), Path, _TRUNCATE);
+
+		seats_Number = 2;
+		seats = new Button_Text[seats_Number];
+
+		seats[0].get_Text().setFont(font);
+		seats[0].get_Text().setString(L"文件读取失败,路径:");
+		seats[0].get_Text().setFillColor(sf::Color(0xFFFFFFFF));
+		seats[0].get_Text().setPosition((float)(1920 / 2), (float)(1080 / 2));
+		seats[0].get_Text().setOrigin(seats[0].get_Text().getGlobalBounds().width / 2, seats[0].get_Text().getGlobalBounds().height); //上对齐
+		seats[0].init();
+		seats[0].set_Default_Color(sf::Color(0x0));
+		seats[0].set_High_Light_Color(sf::Color(0x33333366));
+
+		seats[1].get_Text().setFont(font);
+		seats[1].get_Text().setString(buffer);
+		seats[1].get_Text().setFillColor(sf::Color(0xFFFFFFFF));
+		seats[1].get_Text().setPosition((float)(1920 / 2), (float)(1080 / 2));
+		seats[1].get_Text().setOrigin(seats[1].get_Text().getGlobalBounds().width / 2, 0.0f);//下对齐
+		seats[1].init();
+		seats[1].set_Default_Color(sf::Color(0x0));
+		seats[1].set_High_Light_Color(sf::Color(0x33333366));
+		return;
+	}
+
+	file.close();
+}
+
+void seats_Clicked(unsigned index)
+{
+	return;
 }
